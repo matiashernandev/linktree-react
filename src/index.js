d805b967418d0b657ffe2cf00fb85ce712ed0bf3
Matías Hernán Arroyo <73903008+nkwaaaa@users.noreply.github.com>
2022-10-12 09:55:43 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import { BrowserRouter, Routes, Route, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginView from "./routes/LoginView";
 import DashboardView from "./routes/DashboardView";
 import EditProfileView from "./routes/EditProfileView";
